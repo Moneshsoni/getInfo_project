dccebf1904b478d8cab4c7ae4fd8fd11aaaeebd6
Moneshsoni <sonimonesh115@gmail.com>
2023-10-15 14:18:11 +0530

    add the devise

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_02_084140) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_08_174620) do
   create_table "details", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.string "refference"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "user_type", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["user_type"], name: "index_users_on_user_type", unique: true
   end
 
 end
